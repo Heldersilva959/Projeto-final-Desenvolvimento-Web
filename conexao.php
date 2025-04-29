e552fcb94bf871b96a8e22d7a1cbcd50b307333d
Helder Silva <Heldersilva959@gmail.com>
2025-04-28 22:21:01 -0300

    mudança no footer

--- conexao.php
+++ conexao.php
@@ -1,9 +1,14 @@
 <?php
+/*
 $hostname = $_ENV['DB_HOST'];
 $bancodedados = $_ENV['DB_NAME'];
 $usuario = $_ENV["DB_USER"];
 $senha = $_ENV["DB_PASS"];
-
+*/
+$hostname = "localhost";
+$bancodedados = "sistema_esc";
+$usuario = "root";
+$senha = "";
 // conectando:
 
 $connection = mysqli_connect($hostname, $usuario, $senha, $bancodedados);
@@ -11,4 +16,3 @@ if (!$connection) { //condição de falha
     die("Falha na conexão: " . mysqli_connect_errno() . " - " . mysqli_connect_error()); // mostrando cod do erro e erro
 }
 
-$valor = $_ENV['DB_HOST'];
