9f5df0f8440c399663cd370bacd1d7c97d94b406
Helder Silva <Heldersilva959@gmail.com>
2025-04-28 21:37:31 -0300

    mudança na conexao

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
-$hostname = "localhost";
-$bancodedados = "sistema_esc";
-$usuario = "root";
-$senha = "";
+$hostname = $_ENV['DB_HOST'];;
+$bancodedados = $_ENV['DB_NAME'];;
+$usuario = "DB_USER";
+$senha = "DB_PASS";
 
 // conectando:
 
@@ -10,3 +10,5 @@ $connection = mysqli_connect($hostname, $usuario, $senha, $bancodedados);
 if (!$connection) { //condição de falha
     die("Falha na conexão: " . mysqli_connect_errno() . " - " . mysqli_connect_error()); // mostrando cod do erro e erro
 }
+
+$valor = $_ENV['DB_HOST'];
